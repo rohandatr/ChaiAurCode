1db0de993f0f6927229a2a87227c095a6ddf2973
Rohan Datar <rohandatar2003@gmail.com>
2024-01-06 20:45:02 +0530

    Conversion of datatypes

---
+++ 3_datatypes.js
@@ -0,0 +1,23 @@
+//conversion 
+
+let score = "Master";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Tony";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanInLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
