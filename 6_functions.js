3073240d3feaa464a958883594af683441d94bad
rohandatr <rohandatar2003@gmail.com>
2024-01-11 12:58:08 +0530

    functions

--- 6_functions.js
+++ 6_functions.js
@@ -1,39 +1,39 @@
-// function addTwoNumbers(number1, number2){
-//     console.log(number1 + number2);
-// }
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
 
-// addTwoNumbers(3,5);
+addTwoNumbers(3,5);
 
-// //-------------------------------
+//-------------------------------
 
-// function addTwoNumbers1(number1, number2){
+function addTwoNumbers1(number1, number2){
     
-//     let result = number1 + number2;
-//     return result
-// }
+    let result = number1 + number2;
+    return result
+}
 
-// const result = addTwoNumbers1(3,5);
-// console.log("Result:", result);
+const result = addTwoNumbers1(3,5);
+console.log("Result:", result);
 
-// //------------------------------------------
-
-// // function loginUserMessage(username){
-// //     return `${username} just logged in`
-// // }
-
-// // console.log(loginUserMessage("Rohan"));
-
-
+//------------------------------------------
 
 // function loginUserMessage(username){
-//     if(!username){
-//         console.log("Please enter a username");
-//         return
-//     }
-//     return `${username} just logged in `
+//     return `${username} just logged in`
 // }
 
-// console.log(loginUserMessage());
+// console.log(loginUserMessage("Rohan"));
+
+
+
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in `
+}
+
+console.log(loginUserMessage());
 
 //Rest operator(...) it takes multiple arguments value for single parameter.
 // It gives output as an array.
