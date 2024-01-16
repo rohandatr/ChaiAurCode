8a0732923a2cb31f473f266032528b345b16293d
rohandatr <rohandatar2003@gmail.com>
2024-01-16 21:12:21 +0530

    code exection

---
+++ 10_callstack.js
@@ -0,0 +1,10 @@
+/* 
+   * Code excution 
+
+   JavaScript Execution Context:
+   When the javascript code is run it has three execution context
+   1) Global Execution Context --> this keyword
+   2) Function Execution Context
+   3) Eval Execution Context
+
+*/
