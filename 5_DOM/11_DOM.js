e19a68b717a0c84bd8e10c019a459adebee37749
rohandatr <rohandatar2003@gmail.com>
2024-01-17 23:02:40 +0530

    DOM

---
+++ 5_DOM/11_DOM.js
@@ -0,0 +1,24 @@
+document.getElementById("title")
+//<h1 id=​"title" class=​"heading">​DOM Learning on Chai Aur Code​</h1>​
+
+document.getElementById("title").id
+//'title'
+
+document.getElementById("title").claass
+//undefined
+
+document.getElementById("title").className
+//'heading'
+
+document.getElementById("title").getAttribute('id')
+//'title'
+
+document.getElementById('title').setAttribute('class','test')
+//undefined
+
+
+/* 
+    innerHTML will show the tags or styling tags which are used in the content
+    TextContent will show entire content written in the tag even if it is hidden with the help of CSS.
+    innerText it will show the content in the tag but it wont show the text which is hidden with the help of CSS.
+*/
