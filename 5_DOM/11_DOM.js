2b73534934de87b570f83c1c39a90016e7ea3795
rohandatr <rohandatar2003@gmail.com>
2024-01-18 12:47:22 +0530

    DOM

--- 5_DOM/11_DOM.js
+++ 5_DOM/11_DOM.js
@@ -21,4 +21,20 @@ document.getElementById('title').setAttribute('class','test')
     innerHTML will show the tags or styling tags which are used in the content
     TextContent will show entire content written in the tag even if it is hidden with the help of CSS.
     innerText it will show the content in the tag but it wont show the text which is hidden with the help of CSS.
+
+    To convert a nodeList in array we use Array.from()
+    eg: Array.from(tempClassList)
 */
+
+
+document.getElementsByClassName('listed-items')
+
+const tempClass = document.getElementsByClassName('listed-items')
+
+Array.from(tempClass)
+
+const convertedArray = Array.from(tempClass)
+
+convertedArray.forEach(function(li){
+    li.style.color = "orange"
+})
