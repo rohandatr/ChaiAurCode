fad906deef95de0b92ef8a7942bb3360eeec12fe
rohandatr <rohandatar2003@gmail.com>
2024-01-23 19:25:02 +0530

    async

---
+++ 7_Events/1async.js
@@ -0,0 +1,16 @@
+/*
+Default JavaScript is a Synchronous language
+Default JavaScript is a Single threaded language 
+
+Execution Context:
+1) create one line of code at a time
+2) each operation waits for the last one to complete before executing
+
+Block Code 
+Block the flow of prograam --> Read file sync
+
+Non block code 
+does not block execution --> Read file async
+
+
+*/
